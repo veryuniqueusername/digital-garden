751873d2f02a1a062918975a7514912cad65478e
veryuniqueusername <70266105+veryuniqueusername@users.noreply.github.com>
2024-01-28 17:31:19 +0100

    styling might work

---
+++ src/site/scripts/postProcessTikz.js
@@ -0,0 +1,21 @@
+function invertSVGcolors(lang) {
+	let divs = Array.from(
+		document.getElementsByClassName(`block-language-${lang}`)
+	);
+
+	let svgs = [];
+	divs.forEach((element) => {
+		svgs.push(element.children[0]);
+	});
+
+	svgs.forEach((element) => {
+		let inner = element.innerHTML;
+		inner = inner
+			.replaceAll(/("#000"|"black")/g, `"var(--text-normal)"`)
+			.replaceAll(/("#fff"|"white")/g, `"var(--background-primary)"`);
+		element.innerHTML = inner;
+	});
+}
+
+invertSVGcolors('tikz');
+invertSVGcolors('chemfig');
