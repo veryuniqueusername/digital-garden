b8f2edb055878d4ed8a48f5f8acc0f6632cc78d6
veryuniqueusername <70266105+veryuniqueusername@users.noreply.github.com>
2024-01-28 17:00:13 +0100

    defer

--- src/site/scripts/changeElement.js
+++ src/site/scripts/changeElement.js
@@ -1,45 +1,47 @@
-// Tikz
-const classedTikz = Array.from(
-	document.getElementsByClassName(' language-tikz')
-);
+function changeElement() {
+	// Tikz
+	let classedTikz = Array.from(
+		document.getElementsByClassName('language-tikz')
+	);
 
-const presTikz = [];
+	let presTikz = [];
 
-classedTikz.forEach((element) => {
-	if (element.tagName == 'PRE') {
-		presTikz.push(element);
-	}
-});
+	classedTikz.forEach((element) => {
+		if (element.tagName == 'PRE') {
+			presTikz.push(element);
+		}
+	});
 
-presTikz.forEach((element) => {
-	let codeblock = element.children[0];
-	let tikz = codeblock.innerHTML;
-	let script = document.createElement('script');
-	script.innerHTML = tikz;
-	script.className = 'language-tikz';
-	script.type = 'text/tikz';
-	element.replaceWith(script);
-});
+	presTikz.forEach((element) => {
+		let codeblock = element.children[0];
+		let tikz = codeblock.innerHTML;
+		let script = document.createElement('script');
+		script.innerHTML = tikz;
+		script.className = 'language-tikz';
+		script.type = 'text/tikz';
+		element.replaceWith(script);
+	});
 
-// Chemfig
-const classedChem = Array.from(
-	document.getElementsByClassName(' language-chemfig')
-);
+	// Chemfig
+	let classedChem = Array.from(
+		document.getElementsByClassName('language-chemfig')
+	);
 
-const presChem = [];
+	let presChem = [];
 
-classedChem.forEach((element) => {
-	if (element.tagName == 'PRE') {
-		presChem.push(element);
-	}
-});
+	classedChem.forEach((element) => {
+		if (element.tagName == 'PRE') {
+			presChem.push(element);
+		}
+	});
 
-presChem.forEach((element) => {
-	let codeblock = element.children[0];
-	let chem = codeblock.innerHTML;
-	let script = document.createElement('script');
-	script.innerHTML = chem;
-	script.className = 'language-chemfig';
-	script.type = 'text/tikz';
-	element.replaceWith(script);
-});
+	presChem.forEach((element) => {
+		let codeblock = element.children[0];
+		let chem = codeblock.innerHTML;
+		let script = document.createElement('script');
+		script.innerHTML = chem;
+		script.className = 'language-chemfig';
+		script.type = 'text/tikz';
+		element.replaceWith(script);
+	});
+}
