44e12b135ac3c09484730bf7455bc792c452864b
veryuniqueusername <70266105+veryuniqueusername@users.noreply.github.com>
2024-01-28 17:17:22 +0100

    refactor to function for reusability

--- src/site/scripts/changeElement.js
+++ src/site/scripts/changeElement.js
@@ -1,49 +1,28 @@
-function changeElement() {
-	// Tikz
-	let classedTikz = Array.from(
-		document.getElementsByClassName('language-tikz')
-	);
+function changeElement(lang) {
+	// Chemfig
+	let classed = Array.from(document.getElementsByClassName(`language-${lang}`));
 
-	let presTikz = [];
+	let pres = [];
 
-	classedTikz.forEach((element) => {
+	classed.forEach((element) => {
 		if (element.tagName == 'PRE') {
-			presTikz.push(element);
+			pres.push(element);
 		}
 	});
 
-	presTikz.forEach((element) => {
+	pres.forEach((element) => {
 		let codeblock = element.children[0];
 		let tikz = codeblock.innerHTML;
+		let div = document.createElement('div');
+		div.className = `block-language-${chemfig}`;
 		let script = document.createElement('script');
 		script.innerHTML = tikz;
-		script.className = 'language-tikz';
+		script.className = `language-${lang}`;
 		script.type = 'text/tikz';
-		element.replaceWith(script);
-	});
-
-	// Chemfig
-	let classedChem = Array.from(
-		document.getElementsByClassName('language-chemfig')
-	);
-
-	let presChem = [];
-
-	classedChem.forEach((element) => {
-		if (element.tagName == 'PRE') {
-			presChem.push(element);
-		}
-	});
-
-	presChem.forEach((element) => {
-		let codeblock = element.children[0];
-		let chem = codeblock.innerHTML;
-		let script = document.createElement('script');
-		script.innerHTML = chem;
-		script.className = 'language-chemfig';
-		script.type = 'text/tikz';
-		element.replaceWith(script);
+		div.appendChild(script);
+		element.replaceWith(div);
 	});
 }
 
-changeElement();
+changeElement('tikz');
+changeElement('chemfig');
