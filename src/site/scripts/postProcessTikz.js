163c20810bfaf4c5409c43eff1e6f45b4b817fa2
veryuniqueusername <70266105+veryuniqueusername@users.noreply.github.com>
2024-01-29 21:53:50 +0100

    bundling svgo as well i guess

--- src/site/scripts/postProcessTikz.js
+++
@@ -1,7 +0,0 @@
-function invertSVGcolors(svg) {
-	let inner = svg.innerHTML;
-	inner = inner
-		.replaceAll(/("#000"|"black")/g, `"var(--text-normal)"`)
-		.replaceAll(/("#fff"|"white")/g, `"var(--background-primary)"`);
-	svg.innerHTML = inner;
-}
